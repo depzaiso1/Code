0ae1716f8879f9240a05f08b0ff02ead2f30124c
DanGia <giadh1103@gmail.com>
2022-01-22 13:55:53 +0700

    assvcsd

--- C++/1-Array/AddMatrix.cpp
+++ C++/1-Array/AddMatrix.cpp
@@ -24,22 +24,29 @@ Sum of both matrix =
 using namespace std;
 
 int a[3][3], b[3][3];
-int main(){
-    for(int i=0; i<3;i++){
-        for(int j=0;j<3;j++){
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             cin >> a[i][j];
         }
     }
-    for(int i=0; i<3;i++){
-        for(int j=0;j<3;j++){
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             cin >> b[i][j];
         }
     }
-    for(int i=0; i<3;i++){
-        for(int j=0;j<3;j++){
-            cout <<  a[i][j]+b[i][j] << " ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << a[i][j] + b[i][j] << " ";
         }
-        cout <<"\n";
+        cout << "\n";
     }
     return 0;
 }
--- studyinPink/multiple_testcase.cpp
+++ studyinPink/multiple_testcase.cpp
@@ -7,31 +7,31 @@ void solve()
     int EXP1 = -1, EXP2 = -1;
     int M1 = -1, M2 = -1;
     int E1 = -1, E2 = -1, E3 = -1;
-    cin>>HP1>>HP2;
-    cin>>EXP1>>EXP2;
-    cin>>M1>>M2;
-    cin>>E1>>E2>>E3;
+    cin >> HP1 >> HP2;
+    cin >> EXP1 >> EXP2;
+    cin >> M1 >> M2;
+    cin >> E1 >> E2 >> E3;
     cout << firstMeet(EXP1, EXP2, E1) << endl;
-    cout << EXP1 << " "<< EXP2 << endl;
+    cout << EXP1 << " " << EXP2 << endl;
     //Task 2
-    cout << investigateScene(EXP1, EXP2, HP2, M2, E2)<< endl;
-    cout << EXP1 << " "<< EXP2 << " " << HP2 << " " << M2 << endl;
+    cout << investigateScene(EXP1, EXP2, HP2, M2, E2) << endl;
+    cout << EXP1 << " " << EXP2 << " " << HP2 << " " << M2 << endl;
     //Task 3
-    cout << traceLuggage(HP1, EXP1, M1, E3)<< endl;
-    cout << HP1 << " "<< EXP1 << " " << M1 << endl;
+    cout << traceLuggage(HP1, EXP1, M1, E3) << endl;
+    cout << HP1 << " " << EXP1 << " " << M1 << endl;
 }
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int testcase;
-    cin>>testcase;
-   
-    for(int i=0; i<testcase; i++)
+    cin >> testcase;
+
+    for (int i = 0; i < testcase; i++)
     {
-        cout<<"Testcase #"<<i+1<<": \n";
+        cout << "Testcase #" << i + 1 << ": \n";
         solve();
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
