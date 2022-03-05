cfdab1ecd212734e4dce990e2bd16db1154a569c
DanGia <giadh1103@gmail.com>
2022-03-05 17:17:37 +0700

    update quang

---
+++ Assignment 1 - Initial code (last updated at 2345 14022021)-20220302/studyInPink2.h
@@ -0,0 +1,227 @@
+/*
+* Ho Chi Minh City University of Technology
+* Faculty of Computer Science and Engineering
+* Initial code for Assignment 1
+* Programming Fundamentals Spring 2022
+* Author: Vu Van Tien
+* Date: 10.02.2022
+*/
+
+//The library here is concretely set, students are not allowed to include any other libraries.
+#ifndef studyInPink_h
+#define studyInPink_h
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////
+///STUDENT'S ANSWER BEGINS HERE
+///Complete the following functions
+///DO NOT modify any parameters in the functions.
+////////////////////////////////////////////////////////////////////////
+
+
+long long primeabove(long long &num) {
+    int check = 0;
+    for(int i = 1; i <= sqrt(num); i++) {
+        if(num % i == 0){
+            check++;
+        }
+    }
+    while(check != 2) {
+        num++;
+        check = 0;
+        for(int i = 1; i <= sqrt(num); i++) {
+            if(num % i == 0){
+                check++;
+            }
+        }
+    }
+    return num;
+}
+
+long long fibobelow(long long &num) {
+    long long fibo[500];
+    fibo[0] = 1;
+    fibo[1] = 1;
+    for(int i = 2; i < 100; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    for(int i = 0; i < 500; i++) {
+        if(num <= fibo[i]) {
+            num = fibo[i];
+            break;
+        }
+    }
+    return num;
+}
+
+string notebook1(string ntb1) {
+    // Complete this function to gain point
+    ifstream quang(ntb1);
+    int num;
+    string str;
+    quang>>str;
+    int check = 0;
+    int n1 = 0;
+    string s = "0000000000";
+    for(int i = 11; i <= 13; i++) {
+        if(!(str[i] >= '0' && str[i] <= '9')) {
+            check++;
+        }
+        n1 += (str[i] - 48) * pow(10, 13 - i);
+    }
+    if(check != 0 || n1 == 0) {
+        return "0000000000";
+    }
+    for(int i = 0; i < n1; i++) {
+        quang >> num;
+        s[num] += 1;
+        if(s[num] > '9') {
+            s[num] -= 10;
+        }
+    }
+    quang.close();
+    return s;
+}
+
+string notebook2(string ntb2) {
+    // Complete this function to gain point
+    ifstream quang(ntb2);
+    string str;
+    string data;
+    int check = 0;
+    int n2 = 0;
+    string s1 = "Pink";
+    string s2 = "pink";
+    int pos = 0;
+    long long cntP = 0;
+    quang >> str;
+    for(int i = 0; i < 5; i++) {
+        if(!(str[i] >= '0' && str[i] <= '9')) {
+            check++;
+        }
+        n2 += (str[i] - 48) * pow(10, 4-i);
+    }
+    if(check != 0 || !(n2 >= 5 && n2 <= 100)) {
+        return "1111111111";
+    }
+    for(long long i = 0; i <= n2; i++) {
+        getline(quang, data);
+        string s3 = data;
+        while(s3.find(s1) != string::npos) {
+            pos += s3.find(s1);
+            cntP++;
+            s3 = data.substr(pos + s1.length());
+            pos += s1.length();
+        }
+        pos = 0;
+        s3 = data;
+        while(s3.find(s2) != string::npos) {
+            pos += s3.find(s2);
+            cntP++;
+            s3 = data.substr(pos + s2.length());
+            pos += s2.length();
+        }
+        pos = 0;
+    }
+    if(cntP <= 10000) {
+        cntP = pow(cntP, 2);
+    }
+    while(cntP < 1000000000) {
+        cntP = cntP * 10 + 9;
+    }
+    string s = to_string(cntP);
+    quang.close();
+    return s;
+}
+
+string notebook3(string ntb3) {
+    // Complete this function to gain point
+    ifstream quang(ntb3);
+    string str;
+    long long num;
+    long long arr[10][10];
+    long long temp = 0;
+    long long res = 0;
+    int pos = 0;
+    for(int i = 0; i < 10; i++) {
+        getline(quang, str);
+        for(int j = 0; j < str.size(); j++) {
+            if(str[i] == '|' || str[i] == '-') {
+                str[i] = ' ';
+            }
+        }
+        stringstream ss(str);
+        int p = 0;
+        while(ss >> num) {
+            arr[i][p] = num;
+            p++;
+        }
+        
+    }
+    for(int i = 0; i < 10; i++) {
+        if(i > 0) {
+            primeabove(arr[i-1][i]);
+        }
+        if(i < 9) {
+            fibobelow(arr[i+1][i]);
+        }
+    }
+    for(int i = 0; i < 10; i++) {
+        for(int k = 0; k < 2; k++) {
+            for(int j = 7; j < 9; j++) {
+                if(arr[i][j] >= arr[i][j+1]) {
+                    temp = arr[i][j];
+                    arr[i][j] = arr[i][j+1];
+                    arr[i][j+1] = temp;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++) {
+        long long max = arr[i][0];
+        for(int j = 0; j < 10; j++) {
+            if(arr[i][j] >= max) {
+                max = arr[i][j];
+                pos = j;
+            }
+        }
+        res = res * 10 + pos;
+    }
+    string s = to_string(res);
+    quang.close();
+    return s;
+}
+
+string generateListPasswords(string pwd1, string pwd2, string pwd3) {
+    // Complete this function to gain point
+    return "000000000";
+}
+
+bool chaseTaxi(
+    int arr[100][100],
+    string points,
+    string moves,
+    string & outTimes,
+    string & outCatchUps
+) {
+    // Complete this function to gain point
+    return false;
+}
+
+string enterLaptop(string tag, string message) {
+    // Complete this function to gain point
+    return "";
+}
+
+////////////////////////////////////////////////
+///END OF STUDENT'S ANSWER
+////////////////////////////////////////////////
+#endif /* studyInPink_h */
